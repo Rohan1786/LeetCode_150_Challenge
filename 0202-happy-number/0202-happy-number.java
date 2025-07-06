ec17187b1f674451bab9bfc95792fe5b45c3af13
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-07-06 07:51:27 +0530

    Time: 0 ms (100%), Space: 40.7 MB (64.63%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,42 @@
-class Solution {
-   public int getSquare(int n){
-    int sum =0;
-    while(n!=0){
-        int digit = n % 10;
-        sum+=(digit*digit);
-        n/=10;
-    }
-    return sum;
-   }
-    public boolean isHappy(int n) {
+// class Solution {
+//    public int getSquare(int n){
+//     int sum =0;
+//     while(n!=0){
+//         int digit = n % 10;
+//         sum+=(digit*digit);
+//         n/=10;
+//     }
+//     return sum;
+//    }
+//     public boolean isHappy(int n) {
         
-        Set<Integer>map = new HashSet<>();
-        while(n!=1){
-            if(map.contains(n)) return false;
-            map.add(n);
-            n= getSquare(n);
+//         Set<Integer>map = new HashSet<>();
+//         while(n!=1){
+//             if(map.contains(n)) return false;
+//             map.add(n);
+//             n= getSquare(n);
+//         }
+//         return true;
+//     }
+// }
+
+class Solution {
+    public boolean isHappy(int n) {
+        if (n == 1) return true;
+
+        int value = 0;
+        int temp = n;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            value += digit * digit;
+            temp /= 10;
         }
-        return true;
+
+        // base condition correction:
+        if (value == 1) return true;
+        if (value == n || value == 4) return false; // 4 is part of unhappy cycle
+
+        return isHappy(value);
     }
 }
