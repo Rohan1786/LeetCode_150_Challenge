2f8765faa0ea8f0c32ac9b240f5739462d38ba62
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-07-09 17:36:38 +0530

    Time: 1 ms (100%), Space: 42.6 MB (48.28%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int myAtoi(String s) {
+        s = s.trim();
+        if (s.length() == 0) return 0;
+
+        int sign = 1;
+        long ans = 0;
+        int i = 0;
+        if (s.charAt(0) == '-') {
+            sign = -1;
+            i++;
+        } else if (s.charAt(0) == '+') {
+            i=1;
+        }
+
+        int max = Integer.MAX_VALUE;
+        int min = Integer.MIN_VALUE;
+
+        while (i < s.length()) {
+            if (!Character.isDigit(s.charAt(i))) {
+            break;
+            }
+            ans = ans * 10 + (s.charAt(i) - '0');
+
+            if (sign == 1 && ans > max) return max;
+            if (sign == -1 && -1 * ans < min) return min;
+
+            i++;
+        }
+
+        return (int)(ans * sign);
+    }
+}
