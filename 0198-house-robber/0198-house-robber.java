87cb5eb6c425a59d5ffa592d4e9a2faeaa6465bc
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-09-04 18:04:56 +0530

    Time: 0 ms (100%), Space: 41.1 MB (46.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,20 +1,15 @@
 class Solution {
-    public int choryKaro(int[] nums, int i, int[] memo) {
-        if (i >= nums.length) return 0; // base case
-        
-        if (memo[i] != -1) return memo[i]; // memoization check
-  
-        int skip = choryKaro(nums, i + 1, memo);
-        int rob = nums[i] + choryKaro(nums, i + 2, memo);
-        
-        return memo[i] = Math.max(skip, rob);
-    }
-
     public int rob(int[] nums) {
         int n = nums.length;
-        int[] memo = new int[n];
-        Arrays.fill(memo, -1);
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        int []dp= new int[n];
+        dp[0]=nums[0];
+        dp[1]=Math.max(nums[0],nums[1]);
         
-        return choryKaro(nums, 0, memo); // start from first house
+        for(int i=2; i<n; i++){
+            dp[i]=Math.max(dp[i-1], nums[i]+dp[i-2]);
+        }
+        return dp[n-1];
     }
 }
