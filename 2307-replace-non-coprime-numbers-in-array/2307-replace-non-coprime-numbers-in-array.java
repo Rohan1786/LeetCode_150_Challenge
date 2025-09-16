3a2b16e7b63e8fdb9a775aba1434a42b20b9c57c
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-09-16 13:02:20 +0530

    Time: 113 ms (14.43%), Space: 64.6 MB (36.08%) - LeetHub

--- 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
+++ 2307-replace-non-coprime-numbers-in-array/2307-replace-non-coprime-numbers-in-array.java
@@ -1,35 +1,29 @@
 class Solution {
-    private int gcd(int a, int b) {
-        if (b == 0) return a;
-        return gcd(b, a % b);
+    private int gcd(int a, int b){
+   if(b==0) return a;
+   return gcd(b, a % b);
     }
-
-    private int lcm(int a, int b) {
-        return a / gcd(a, b) * b;
-    }
-
+    private int lcm(int a, int b){
+        return a/gcd(a,b) * b;}
     public List<Integer> replaceNonCoprimes(int[] nums) {
         Stack<Integer> stack = new Stack<>();
-
-        for (int num : nums) {
+        for(int num: nums){
             stack.push(num);
+          while (stack.size() > 1) {
+    int a = stack.pop();
+    int b = stack.pop();
+    int g = gcd(a, b);
+    if (g > 1) {
+        stack.push(lcm(a, b));  // merge
+    } else {
+        stack.push(b);
+        stack.push(a);
+        break; // stop if coprime
+    }
+}
 
-            // merge while top two are not coprime
-            while (stack.size() > 1) {
-                int a = stack.pop();
-                int b = stack.pop();
-
-                int g = gcd(a, b);
-                if (g > 1) {  // not coprime → merge
-                    stack.push(lcm(a, b));
-                } else {
-                    stack.push(b);
-                    stack.push(a);
-                    break;
-                }
-            }
+            
         }
-
         return new ArrayList<>(stack);
     }
 }
