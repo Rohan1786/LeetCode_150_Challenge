2a254e89f739949a29c0aed03805fa7d28dd33fa
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-09-22 11:18:00 +0530

    Time: 0 ms (100%), Space: 44.5 MB (82.41%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,54 @@
 class Solution {
-public void helper(int[] nums, List<Integer> arr,boolean[] isVisited, List<List<Integer>>ans)
-{
-     int n = nums.length;
-    if(arr.size()==n) {
+// public void helper(int[] nums, List<Integer> arr,boolean[] isVisited, List<List<Integer>>ans)
+// {
+//      int n = nums.length;
+//     if(arr.size()==n) {
+//         List<Integer> list = new ArrayList<>();
+//         for(int i=0; i<n; i++) {
+//             list.add(arr.get(i));
+//         }
+//         ans.add(list);
+//         return;
+//     }
+//     for(int i=0; i<n; i++) {
+//         if(!isVisited[i]){
+//             arr.add(nums[i]);
+//             isVisited[i]=true;
+//             helper(nums,arr,isVisited,ans);
+//             arr.remove(arr.size()-1);
+//             isVisited[i]=false;
+//         }
+//     }
+// }   
+
+public void helper1(int[] nums, int idx, List<List<Integer>>ans){
+    int n = nums.length;
+    if(n==idx){
         List<Integer> list = new ArrayList<>();
-        for(int i=0; i<n; i++) {
-            list.add(arr.get(i));
+        for(int i=0; i<n; i++){
+            list.add(nums[i]);
         }
         ans.add(list);
         return;
     }
-    for(int i=0; i<n; i++) {
-        if(!isVisited[i]){
-            arr.add(nums[i]);
-            isVisited[i]=true;
-            helper(nums,arr,isVisited,ans);
-            arr.remove(arr.size()-1);
-            isVisited[i]=false;
-        }
+    for(int i=idx; i<n; i++){
+        swap(i, idx, nums);
+        helper1(nums,idx+1,ans);
+        swap(i, idx, nums);
     }
-}    public List<List<Integer>> permute(int[] nums) {
+}
+public void swap(int i, int idx, int[]nums){
+    int temp = nums[i];
+    nums[i]=nums[idx];
+    nums[idx]=temp;
+}
+ public List<List<Integer>> permute(int[] nums) {
     List<List<Integer>> ans = new ArrayList<>();
     int n = nums.length;
-    List<Integer>arr = new ArrayList<>();
-    boolean[] isValid = new boolean[n];
-    helper(nums, arr, isValid, ans);
+    // List<Integer>arr = new ArrayList<>();
+    // boolean[] isValid = new boolean[n];
+    // helper(nums, arr, isValid, ans);
+    helper1(nums, 0, ans);
     return ans;
     }
 }
