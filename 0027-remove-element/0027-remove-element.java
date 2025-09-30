b97f8b4661d5eec5d368c0b8fd3e78e66815452b
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-09-30 15:05:11 +0530

    Time: 0 ms (100%), Space: 42.5 MB (9.17%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,41 +1,11 @@
 class Solution {
-    // Bubble sort in descending order
-    public void bubbleSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length - i - 1; j++) {
-                if (arr[j] < arr[j + 1]) {  // Sorting in descending order
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-  
     public int removeElement(int[] nums, int val) {
-        int count = 0;
-
-       
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val) {
-                nums[count] = nums[i];  
-                count++;                
+        int index =0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i]!=val){
+                nums[index++]=nums[i];
             }
         }
-
-       
-        int[] sortedArray = new int[count];
-        for (int i = 0; i < count; i++) {
-            sortedArray[i] = nums[i];
-        }
-        bubbleSort(sortedArray);
-
-        // Optionally, copy the sorted array back to `nums`
-        for (int i = 0; i < count; i++) {
-            nums[i] = sortedArray[i];
-        }
-
-        return count;  // Return the number of valid elements after removal
+        return index;
     }
 }
