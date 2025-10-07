9252eeed06834508eeba6677c6a52653b4ed1830
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-07 19:58:01 +0530

    Time: 27 ms (8.83%), Space: 53.5 MB (12.37%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -7,15 +7,17 @@ class Solution {
     if(t[i][amount]!=-1) return t[i][amount];
     if(i==n) return 0;
     if(coins[i]>amount){
-        return solve(i+1, amount, coins);
+        return t[i][amount]=solve(i+1, amount, coins);
     }
 int take = solve(i, amount-coins[i], coins);
 int skip = solve(i+1, amount,coins);
 return t[i][amount]=take+skip;
     }
     public int change(int amount, int[] coins) {
-        t=new int[301][5001];
-        for(int i=0;i<301; i++){
+        int n = coins.length;
+    
+        t=new int[n+1][amount+1];
+        for(int i=0;i<n+1; i++){
             Arrays.fill(t[i],-1);
         }
         return solve(0, amount, coins);
