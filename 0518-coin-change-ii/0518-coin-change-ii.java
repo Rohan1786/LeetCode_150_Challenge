b66931e1d5c8eb9626308c7b4940ff53b669db2b
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-07 19:39:16 +0530

    Time: 218 ms (5.02%), Space: 60.3 MB (5.03%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,36 +1,23 @@
 class Solution {
-    // int[][] memo = new int[301][5001]; 
+    int[][]t;
+    public int solve(int i, int amount, int[] coins){
     
-    // public int solve(int i, int[] coins, int amount) {
-    //     int n = coins.length;
-        
-    //     if (amount == 0) return 1;   
-    //     if (i == n) return 0;      
-        
-    //     if (memo[i][amount] != -1) return memo[i][amount];
-        
-    //     int take = 0;
-    //     if (coins[i] <= amount) {
-    //         take = solve(i, coins, amount - coins[i]); 
-    //     }
-        
-    //     int skip = solve(i + 1, coins, amount); 
-        
-    //     return memo[i][amount] = take + skip;
-    // }
-    
+    int n = coins.length;
+    if(amount ==0) return 1;
+    if(t[i][amount]!=-1) return t[i][amount];
+    if(i==n) return 0;
+    if(coins[i]>amount){
+        return solve(i+1, amount, coins);
+    }
+int take = solve(i, amount-coins[i], coins);
+int skip = solve(i+1, amount,coins);
+return t[i][amount]=take+skip;
+    }
     public int change(int amount, int[] coins) {
-        // for (int[] arr : memo) {
-        //     Arrays.fill(arr, -1);
-        // }
-        // return solve(0, coins, amount);
-        int[] dp =  new int[amount+1];
-        dp[0]=1;
-        for(int coin: coins){
-            for(int a=coin; a<=amount; a++){
-                dp[a] += dp[a-coin];
-            }
+        t=new int[301][5001];
+        for(int i=0;i<301; i++){
+            Arrays.fill(t[i],-1);
         }
-        return dp[amount];
+        return solve(0, amount, coins);
     }
 }
