a03fbbd15689ea0afd47dcbff57ad633c318ea3c
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-08 07:56:06 +0530

    Time: 7 ms (63.86%), Space: 48 MB (56.78%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -2,24 +2,37 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         //26 chars 
         
-         HashMap<String,List<String>> map = new LinkedHashMap<>();
+        //  HashMap<String,List<String>> map = new LinkedHashMap<>();
         
+        // for(String name: strs) {
+        //     StringBuilder res = new StringBuilder();
+        //      int[] field = new int[26];
+        //      for(char ch: name.toCharArray()) {
+        //         field[ch-'a']++;
+        //      }
+        //      for(int num: field) {
+        //         res.append(num).append("#");
+        //      }
+        //       String key = res.toString();
+        //     if(!map.containsKey(key)) {
+        //         map.put(key, new ArrayList<>());
+        //     }
+        //     map.get(key).add(name);
+        // }
+        //   List<List<String>> result = new ArrayList<>(map.values());
+        // return result;
+
+        HashMap<String, List<String>> map = new HashMap<>();
         for(String name: strs) {
-            StringBuilder res = new StringBuilder();
-             int[] field = new int[26];
-             for(char ch: name.toCharArray()) {
-                field[ch-'a']++;
-             }
-             for(int num: field) {
-                res.append(num).append("#");
-             }
-              String key = res.toString();
-            if(!map.containsKey(key)) {
-                map.put(key, new ArrayList<>());
+            char[] chArr = name.toCharArray();
+            Arrays.sort(chArr);
+            String res = new String(chArr);
+            if(!map.containsKey(res)) {
+                map.put(res, new ArrayList<>());
             }
-            map.get(key).add(name);
+            map.get(res).add(name);
         }
-          List<List<String>> result = new ArrayList<>(map.values());
+        List<List<String>> result = new ArrayList<>(map.values());
         return result;
     }
 }
