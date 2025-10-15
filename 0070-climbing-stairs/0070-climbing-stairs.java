401e69f9a0450f8525698a607bb9a92ff51e6847
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-15 16:52:38 +0530

    Time: 0 ms (100%), Space: 41 MB (5.78%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,23 @@
 class Solution {
     int[] memo;
     public int climbStairs(int n) {
-        memo = new int[n+1];
-       Arrays.fill(memo, -1);
-       return climb(n);
+    //     memo = new int[n+1];
+    //    Arrays.fill(memo, -1);
+       //now its time to make bottom up approach
+       if(n==0||n==1) return 1;
+       int[] bottom = new int[n+1];
+       bottom[0]=1;
+       bottom[1]=1;
+       for(int i=2; i<=n; i++){
+        //0 1 2 
+        bottom[i]=bottom[i-1]+bottom[i-2];
+       }
+       //return memo[n];
+       return bottom[n];
     }
-    public int climb(int n){
- if(n==0||n==1) return 1;
-        if(memo[n]!=-1) return memo[n];
-      return memo[n]=climb(n-1)+climb(n-2);
-    }
+//     public int climb(int n){
+//  if(n==0||n==1) return 1;
+//         if(memo[n]!=-1) return memo[n];
+//       return memo[n]=climb(n-1)+climb(n-2);
+//     }
 }
