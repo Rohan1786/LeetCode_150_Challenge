35090a2411f1d20d7927f41f3cd03f5ea898deff
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-17 17:25:45 +0530

    Time: 13 ms (50.06%), Space: 49.2 MB (31.39%) - LeetHub

--- 0744-network-delay-time/0744-network-delay-time.java
+++ 0744-network-delay-time/0744-network-delay-time.java
@@ -1,37 +1,23 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-       HashMap<Integer, List<int[]>> graph = new HashMap<>(); // Change the value type
-for (int[] edge : times) {
-    graph.computeIfAbsent(edge[0], x -> new ArrayList<>()).add(new int[]{edge[1], edge[2]});
-}
-        PriorityQueue<int[]> pq= new PriorityQueue<>(Comparator.comparingInt(a->a[1]));
-        pq.offer(new int[]{k,0});
-        int[] distances= new int[n+1];
-        Arrays.fill(distances, Integer.MAX_VALUE);
-        distances[k]=0;
-        while(!pq.isEmpty()) {
-    int[] current= pq.poll();
-    int currentNode = current[0];
-    int currentDist = current[1];
-    if(currentDist>distances[currentNode]) {
-        continue;
-
-    }
-
-    if(graph.containsKey(currentNode)){
-          for(int[] neighbour : graph.get(currentNode)) {
-            int nextNode = neighbour[0];
-            
-            int nextDist = currentDist+neighbour[1];
-            if(nextDist<distances[nextNode]) {
-                distances[nextNode]=nextDist;
-                pq.offer(new int[]{nextNode,nextDist});
+        int[]dist= new int[n];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[k-1]=0;
+        for(int i=0; i<n-1; i++) {
+            for(int[] e: times){
+            int u = e[0]-1;
+            int v = e[1]-1;
+            int wt = e[2];
+           if(dist[u]!=Integer.MAX_VALUE && dist[u]+wt<dist[v]) {
+            dist[v]=dist[u]+wt;
+           }
             }
-
-          }
-    }
         }
-        int max=Arrays.stream(distances).skip(1).max().getAsInt();
-        return max==Integer.MAX_VALUE?-1:max;
-            }
+        int max =0;
+        for(int num: dist) {
+            if(num==Integer.MAX_VALUE) return -1;
+            max=Math.max(max, num);
+        }
+        return max;
+    }
 }
