1e51c2e78326fb818778e22876c5037740029c0e
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-22 23:55:57 +0530

    Time: 35 ms (6.44%), Space: 53 MB (27.5%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,24 +1,24 @@
 class Solution {
+    int n;
     int[][]t;
-    public int solve(int i, int amount, int[] coins){
-    
-    int n = coins.length;
-    if(amount ==0) return 1;
-    if(t[i][amount]!=-1) return t[i][amount];
-    if(i==n) return 0;
-    if(coins[i]>amount){
-        return t[i][amount]=solve(i+1, amount, coins);
-    }
-int take = solve(i, amount-coins[i], coins);
-int skip = solve(i+1, amount,coins);
-return t[i][amount]=take+skip;
+   public int solve(int i, int amount, int[] coins) {
+       
+        if (amount == 0) return 1;
+ 
+        if (i >= n || amount < 0) return 0;
+ 
+        if (t[i][amount] != -1) return t[i][amount];
+  
+        int take = solve(i, amount - coins[i], coins);  
+        int skip = solve(i + 1, amount, coins);           
+        return t[i][amount] = take + skip;
     }
     public int change(int amount, int[] coins) {
-        int n = coins.length;
-    
+        n = coins.length;
         t=new int[n+1][amount+1];
-        for(int i=0;i<n+1; i++){
-            Arrays.fill(t[i],-1);
+        for(int[]arr: t)
+        {
+            Arrays.fill(arr,-1);
         }
         return solve(0, amount, coins);
     }
