d508a1fd65a159ce107b70699548c7e28e6c28e1
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-22 11:45:57 +0530

    Time: 0 ms (100%), Space: 40.3 MB (80.47%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,17 +1,28 @@
 class Solution {
-    int[] dp;
+    int[] memo;
     public int count_unique(int n ) {
        if(n<=1) return 1;
-       if(dp[n]!=-1) return dp[n];
+       if(memo[n]!=-1) return memo[n];
        int total=0;
        for(int i=1; i<=n; i++){
      total+=count_unique(i-1)*count_unique(n-i);
        }
-       return dp[n]=total;
+       return memo[n]=total;
     }
     public int numTrees(int n) {
-        dp=new int[20];
-       Arrays.fill(dp, -1);
-        return count_unique(n);
+    //     memo=new int[20];
+    //    Arrays.fill(memo, -1);
+    //     return count_unique(n);
+//Now we will go with the dp
+int[]dp = new int[n+1];
+dp[0]=1;
+dp[1]=1;
+for(int i=2; i<=n; i++)
+{
+    for(int j=1; j<=i; j++){
+         dp[i] += dp[j - 1] * dp[i - j];
+    }
+}
+return dp[n];
     }
 }
