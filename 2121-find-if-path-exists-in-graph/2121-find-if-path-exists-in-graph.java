19e0bfe911ecdf6185f735b6a130f8ba00390346
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-10-23 18:59:07 +0530

    Time: 285 ms (13.26%), Space: 206.4 MB (8.42%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.java
@@ -1,31 +1,27 @@
 class Solution {
-    public boolean validPath(int n, int[][] edges, int source, int destination) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            graph.add(new ArrayList<>());
-        }
-        for(int[] arr: edges){
-            int u =arr[0];
-            int v= arr[1];
-            graph.get(u).add(v);
-            graph.get(v).add(u);
-        }
-        boolean []vis = new boolean[n];
-        Stack<Integer> stack =new Stack<>();
-        stack.push(source);
-        while(!stack.isEmpty()){
-            int node = stack.pop();
-            if (node == destination) return true;
-            if (vis[node]) continue;
-            vis[node] = true;
-                
-            for(int neigh: graph.get(node)){
-                if(!vis[neigh]){
-                 
-                    stack.push(neigh);
+    public boolean dfs(int source, int dist,Map<Integer, List<Integer>> graph, boolean[] visited) {
+
+        if(source == dist) return true;
+        visited[source]=true;
+        for(int node: graph.get(source)) {
+             if(!visited[node]) {
+                if(dfs(node, dist, graph, visited)){
+                    return true;
                 }
-            }
+             }
         }
-   return false;
+        return false;
+    }
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+    Map<Integer, List<Integer>> graph = new LinkedHashMap<>();
+    for(int i=0; i<n; i++){
+        graph.putIfAbsent(i, new ArrayList<>());
+    }
+    for(int arr[]: edges) {
+        graph.get(arr[0]).add(arr[1]);
+        graph.get(arr[1]).add(arr[0]);
+    }
+    boolean [] visited=new boolean[n];
+    return dfs(source, destination, graph, visited);
     }
 }
