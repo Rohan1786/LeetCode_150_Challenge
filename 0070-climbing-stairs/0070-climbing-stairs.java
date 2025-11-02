261f8a2d233589c262004d9a9e84db1216514c95
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-02 13:21:18 +0530

    Time: 0 ms (100%), Space: 41.9 MB (5.66%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -33,14 +33,30 @@ int[] memo;
         // Arrays.fill(memo, -1);
         // return memoTech(n, 0);
         //now the dp approach 
-        if (n == 0) return 0;
+        // if (n == 0) return 0;
+        // if (n == 1) return 1;
+        // int[] dp  = new int[n+1];
+        // dp[0]=1;
+        // dp[1]=1;
+        // for(int i=2; i<=n; i++){
+        //     dp[i]=dp[i-1]+dp[i-2];
+        // }
+        // return dp[n];+
+
+
+        // now the tricky approach
+         if (n == 0) return 0;
         if (n == 1) return 1;
-        int[] dp  = new int[n+1];
-        dp[0]=1;
-        dp[1]=1;
-        for(int i=2; i<=n; i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
-        return dp[n];
+        int i=2;
+        int c = 0;
+        int a =1;
+        int b=1;
+       while(i<=n){
+        c=a+b;
+        a=b;
+        b=c;
+        i++;
+       }
+       return c;
     }
 }
