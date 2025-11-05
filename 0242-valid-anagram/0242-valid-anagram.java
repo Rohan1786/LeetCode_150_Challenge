186a8600a5de6c4b2aef8adf132a90ed097374fd
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-05 16:53:53 +0530

    Time: 2 ms (97.79%), Space: 44.5 MB (49.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,21 +1,37 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map = new HashMap<>();
+        // Map<Character,Integer> map = new HashMap<>();
         int n = s.length();
         int tn = t.length();
         if(n!=tn) return false;
-        for(char ch: s.toCharArray()){
-            map.put(ch, map.getOrDefault(ch, 0)+1);
-        }
-        for(char ch: t.toCharArray()) {
-            if(!map.containsKey(ch)) {
-                return false;
-            }
-            map.put(ch, map.get(ch)-1);
-            if(map.get(ch)==0){
-                map.remove(ch);
-            }
-        }
-        return map.isEmpty();
+        // for(char ch: s.toCharArray()){
+        //     map.put(ch, map.getOrDefault(ch, 0)+1);
+        // }
+        // for(char ch: t.toCharArray()) {
+        //     if(!map.containsKey(ch)) {
+        //         return false;
+        //     }
+        //     map.put(ch, map.get(ch)-1);
+        //     if(map.get(ch)==0){
+        //         map.remove(ch);
+        //     }
+        // }
+        // return map.isEmpty();
+
+        //now for the constant solutin
+   int[] field=new int[26];
+   for(char ch: s.toCharArray()) {
+    field[ch-'a']++;
+   }
+   for(char ch: t.toCharArray()){
+      field[ch-'a']--;
+   }
+   for(int i=0; i<26; i++){
+    if(field[i]!=0){
+      return false;
+    }
+   }
+   return true;
+        
     }
 }
