7bd368a36e4c2956ab3b09d0a9d4c14259b3243e
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-18 23:11:03 +0530

    Time: 0 ms (100%), Space: 19.1 MB (10.46%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        freq={}
+        for i in range(0, len(nums)):
+            complement = target - nums[i]
+
+            if complement in freq:
+                return [freq[complement], i]
+            freq[nums[i]]=i
+        return {}
