5cb913f8cdfe9b4c965fd478c68327bdbaf1fdd0
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-18 22:50:52 +0530

    Time: 53 ms (68.47%), Space: 17.9 MB (82.93%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,10 +1,16 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        field=[0]*26
-        for ch in s:
-            field[ord(ch)-ord('a')]+=1
-        for i,ch in enumerate(s):
-            if(field[ord(ch)-ord('a')]==1):
+        # field=[0]*26
+        # for ch in s:
+        #     field[ord(ch)-ord('a')]+=1
+        # for i,ch in enumerate(s):
+        #     if(field[ord(ch)-ord('a')]==1):
+        #         return i
+        # return -1
+
+        freq= Counter(s)
+        for i, ch in enumerate(s):
+            if(freq[ch]==1):
                 return i
         return -1
 
