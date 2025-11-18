798b52fe19584f731254c4b86289810d51be2d52
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-18 22:53:42 +0530

    Time: 32 ms (60.62%), Space: 58.4 MB (76.83%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,15 +3,28 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let field = new Array(26).fill(0);
-    for(ch of s){
-        field[ch.charCodeAt(0)-'a'.charCodeAt(0)]++;
+//     let field = new Array(26).fill(0);
+//     for(ch of s){
+//         field[ch.charCodeAt(0)-'a'.charCodeAt(0)]++;
+//     }
+//    for (let i = 0; i < s.length; i++) {
+//         let ch = s[i];
+//         if (field[ch.charCodeAt(0) - 'a'.charCodeAt(0)] === 1) {
+//             return i;
+//         }
+//     }
+// return -1;
+
+let freq={}
+for(ch of s){
+    freq[ch]=(freq[ch]||0)+1
+}
+for(let i=0; i<s.length; i++){
+    let ch=s[i];
+    if(freq[ch]===1){
+        return i;
     }
-   for (let i = 0; i < s.length; i++) {
-        let ch = s[i];
-        if (field[ch.charCodeAt(0) - 'a'.charCodeAt(0)] === 1) {
-            return i;
-        }
-    }
+
+}
 return -1;
 };
