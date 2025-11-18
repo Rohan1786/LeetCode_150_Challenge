3a80bc41dfb0244b876589dfb554d97176d60400
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-18 23:15:11 +0530

    Time: 1 ms (87.54%), Space: 57.8 MB (26.31%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let freq={}
+    for(let i=0; i<nums.length; i++){
+        let complement= target-nums[i];
+        if (freq.hasOwnProperty(complement)){
+            return  [freq[complement], i];
+        }
+        freq[nums[i]]=i
+    }
+    return []
+};
