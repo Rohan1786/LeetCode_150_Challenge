55ec0e6f996f58a283daaac3252c08a9d42bee09
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-11-26 12:54:11 +0530

    Time: 31 ms (61.08%), Space: 155.8 MB (8.26%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,24 +1,41 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-   PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
-        int n = nums.length;
-        int[] res = new int[n - k + 1];
-        int idx = 0;
+        // int n= nums.length;
+        // int size=n-k;
+        // int[]arr = new int[size+1];
 
-        for (int i = 0; i < n; i++) {
-            pq.add(new int[]{nums[i], i});
+        // int left =0;
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->b-a);
+        // for(int right=0; right<n; right++){
+            
+        //      pq.offer(nums[right]);
+        //       if(pq.size()>=k){
+                
+        //         arr[left]=pq.peek();
+        //       pq.remove(nums[left]);
+        //       left++;
+        //      }  
+        // }
+        // return arr;
+        int n=nums.length;
+        int size=n-k+1;
+        int[] ans =new int[size];
+        Deque<Integer> dq = new LinkedList<>();
+        for(int i=0;i<n; i++){
+            while(!dq.isEmpty() && dq.peek()<i-k+1){
+                dq.poll();
+            
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+                dq.pollLast();
+            }
+            dq.offer(i);
 
-            // Remove elements that are outside the window
-            while (pq.peek()[1] <= i - k) {
-                pq.poll();
+            if(i>=k-1){
+                ans[i-k+1]=nums[dq.peek()];
             }
 
-            // Once we have the first full window, start recording results
-            if (i >= k - 1) {
-                res[idx++] = pq.peek()[0];
-            }
         }
-
-        return res;
+        return ans;
     }
 }
