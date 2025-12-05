35bd43663fca2c4a869df6728fdcc60c696a32c4
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-12-05 10:03:56 +0530

    Time: 5 ms (26.26%), Space: 18.5 MB (53.27%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -9,12 +9,29 @@ class Solution:
 
                 
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[0])):
-                if matrix[i][j]==0:
-                    self.make(matrix, i, j);
+        # for i in range(0, len(matrix)):
+        #     for j in range(0, len(matrix[0])):
+        #         if matrix[i][j]==0:
+        #             self.make(matrix, i, j);
         
-        for i in range(0, len(matrix)):
-            for j in range(0, len(matrix[0])):
-                if matrix[i][j]==float("inf"):
-                    matrix[i][j]=0;
+        # for i in range(0, len(matrix)):
+        #     for j in range(0, len(matrix[0])):
+        #         if matrix[i][j]==float("inf"):
+        #             matrix[i][j]=0;
+
+        n = len(matrix)
+        m = len(matrix[0])
+
+        rows= [False for _ in range(0, n)]
+        cols=[False for _ in range(0,m)]
+        
+        for i in range(0, n):
+            for j in range(0, m):
+                if matrix[i][j]==0:
+                    rows[i]=True
+                    cols[j]=True
+        
+        for i in range(0, n):
+            for j in range(0, m):
+                if rows[i] or cols[j]:
+                    matrix[i][j]=0
