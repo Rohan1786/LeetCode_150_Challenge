9d97f1be7feeefb519019b77c113d943522823f2
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-12-06 10:15:08 +0530

    Time: 0 ms (100%), Space: 43.5 MB (42.03%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,30 +1,24 @@
 class Solution {
-    public void inPlaceSwap(int[][] matrix) {
-        int row = matrix.length;
-        for (int i = 0; i < row; i++) {
-            for (int j = i + 1; j < row; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+    
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+               int temp = matrix[i][j];
+               matrix[i][j]=matrix[j][i];
+               matrix[j][i]=temp;
             }
         }
-    }
-
-    public void reverseMatrix(int[] arr) {
-        int i = 0, j = arr.length - 1;
-        while (i < j) {
-            int swap = arr[i];
-            arr[i] = arr[j];
-            arr[j] = swap;
-            i++;
-            j--;
+        for(int i=0; i<n; i++){
+            int left=0, right=n-1;
+            while(left<right){
+                int temp=matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
         }
-    }
-
-    public void rotate(int[][] matrix) {
-        inPlaceSwap(matrix); // transpose
-        for (int i = 0; i < matrix.length; i++) {
-            reverseMatrix(matrix[i]); // reverse each row
-        }
+        
     }
 }
