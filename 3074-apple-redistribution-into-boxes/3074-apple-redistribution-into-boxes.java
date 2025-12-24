c155abb0969eece9c6ed6db7121697e1966ab4ca
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-12-24 12:38:56 +0530

    Time: 11 ms (5.34%), Space: 44.8 MB (5.34%) - LeetHub

--- 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
+++ 3074-apple-redistribution-into-boxes/3074-apple-redistribution-into-boxes.java
@@ -1,18 +1,34 @@
 class Solution {
     public int minimumBoxes(int[] apple, int[] capacity) {
-        PriorityQueue<Integer> heap = new PriorityQueue<>((a,b)->b-a);
+        // PriorityQueue<Integer> heap = new PriorityQueue<>((a,b)->b-a);
         int sum = Arrays.stream(apple).sum();
-for(int cap: capacity){
-      heap.offer(cap);
-}
+    
+int[] capacityBody = IntStream.of(capacity)
+                         .boxed()
+                         .sorted(Collections.reverseOrder())
+                         .mapToInt(Integer::intValue)
+                         .toArray();
 int cnt =0;
-int max =0;
-while(!heap.isEmpty()){
-       max+=heap.poll();
-        cnt++;
-       if(max>=sum) break;
-      
+for(int i=0; i<capacityBody.length; i++){
+    if(sum<=0){
+        return cnt;
+    }
+    cnt++;
+    sum-=capacityBody[i];
 }
 return cnt;
+// for(int cap: capacity){
+//       heap.offer(cap);
+// }
+
+// int cnt =0;
+// int max =0;
+// while(!heap.isEmpty()){
+//        max+=heap.poll();
+//         cnt++;
+//        if(max>=sum) break;
+      
+// }
+// return cnt;
     }
 }
