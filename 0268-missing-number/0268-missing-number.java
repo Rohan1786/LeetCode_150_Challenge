c84d8e455622f41a03b64362b765b496eb9ad34d
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2025-12-31 17:24:43 +0530

    Time: 3 ms (23.46%), Space: 47.4 MB (30.88%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = Arrays.stream(nums).sum();
+        int n = nums.length;
+        int avg = (n*(n+1))/2;
+        return avg-sum;
+    }
+}
