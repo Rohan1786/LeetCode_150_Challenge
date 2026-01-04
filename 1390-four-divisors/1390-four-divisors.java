d4a8d9afa3f33a5963937256de782f9dfb0eab0f
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-04 22:31:30 +0530

    Time: 29 ms (45.2%), Space: 46.1 MB (91.16%) - LeetHub

--- 1390-four-divisors/1390-four-divisors.java
+++ 1390-four-divisors/1390-four-divisors.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int[] check(int num){
+    public int check(int num){
         int sum =0;
         int cnt =0;
         for(int i=1; i*i<=num; i++){
@@ -13,18 +13,19 @@ class Solution {
                  
             }
         }
-        return new int[]{sum, cnt};
+        return cnt==4?sum:0;
     }
     public int sumFourDivisors(int[] nums) {
         
         int max =0;
         for(int num: nums){
-           int curr[] = check(num);
-           int count = curr[1];
-           int sum = curr[0];
-           if(count == 4){
+        //    int curr[] = check(num);
+        //    int count = curr[1];
+        //    int sum = curr[0];
+         int sum = check(num); 
+         
             max+=sum;
-           }
+           
         }
         return max;
     }
