4fd8e063d85a8f6342d28a3e26fafa61d26f8f4c
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-05 22:12:03 +0530

    Time: 36 ms (12.97%), Space: 47.4 MB (12.23%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,49 +1,29 @@
-import java.util.*;
-
 class Solution {
-    int[][] t;  
+    int[][] memo;
+    int m;
+    final int INF = (int)1e9;
 
-    public int solve(int i, int amount, int[] coins) {
-        int n = coins.length;
+    public int solve(int[] coins, int i, int amount) { 
+        if (amount == 0) return 0; 
+        if (i == m || amount < 0) return INF; 
+        if (memo[i][amount] != -1)
+            return memo[i][amount];
+ 
+        int skip = solve(coins, i + 1, amount);
+ 
+        int take = 1 + solve(coins, i, amount - coins[i]);
 
-        // Base cases
-        if (amount == 0) return 0;          
-        if (i == n) return 1000000000;         
-        if (t[i][amount] != -1) return t[i][amount];
- 
-        if (coins[i] > amount) {
-            return t[i][amount] = solve(i + 1, amount, coins);
-        }
- 
-        int take =  1+solve(i, amount - coins[i], coins);
- 
-        int skip = solve(i + 1, amount, coins);
-
-    
-        return t[i][amount] = Math.min(take, skip);
+        return memo[i][amount] = Math.min(skip, take);
     }
 
     public int coinChange(int[] coins, int amount) {
-        // initialize dp array same as your 518 code
-        // t = new int[301][5001];
-        // for (int i = 0; i < 301; i++) {
-        //     Arrays.fill(t[i], -1);
-        // }
+        m = coins.length;
+        memo = new int[m][amount + 1];
 
-        // int ans = solve(0, amount, coins);
+        for (int[] row : memo)
+            Arrays.fill(row, -1);
 
-        // // if impossible, return -1
-        // return ans >= 1000000000 ? -1 : ans;
-        int[] dp =new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0]=0;
-        for(int i=1; i<=amount; i++){
-            for(int coin: coins){
-                if(i-coin>=0){
-                dp[i]=Math.min(dp[i],dp[i-coin]+1);
-                }
-            }
-        }
-        return dp[amount]>amount?-1:dp[amount];
+        int ans = solve(coins, 0, amount);
+        return ans >= INF ? -1 : ans;
     }
 }
