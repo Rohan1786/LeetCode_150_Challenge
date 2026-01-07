6d8b1da275eb44f69f754ade1defff0283d95d7e
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-07 16:53:02 +0530

    Time: 5 ms (56.58%), Space: 47.6 MB (30.15%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,36 +1,39 @@
 class Solution {
-    private int m, n;
-    private int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
-        m = heights.length;
-        n = heights[0].length;
-        boolean[][] pacific = new boolean[m][n];
-        boolean[][] atlantic = new boolean[m][n];
-        for (int j = 0; j < n; j++) dfs(0, j, heights, pacific);
-        for (int i = 0; i < m; i++) dfs(i, 0, heights, pacific);
-        for (int j = 0; j < n; j++) dfs(m - 1, j, heights, atlantic);
-        for (int i = 0; i < m; i++) dfs(i, n - 1, heights, atlantic);
-
-        List<List<Integer>> result = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (pacific[i][j] && atlantic[i][j]) {
-                    result.add(Arrays.asList(i, j));
+        int row = heights.length;;
+        int col = heights[0].length;
+        boolean[][] pacificReachable = new boolean[row][col];
+        boolean[][] atlanticReachable = new boolean[row][col];
+        for(int i=0; i<row; i++){ 
+               dfs(i, 0, pacificReachable, heights);
+               dfs(i, col-1, atlanticReachable, heights); 
+        }
+        for(int i=0; i<col; i++){
+            dfs(0, i, pacificReachable, heights);
+            dfs(row-1, i, atlanticReachable, heights);
+        }
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i=0; i<row; i++){
+            for(int j=0; j<col; j++){
+                if(pacificReachable[i][j] && atlanticReachable[i][j]){
+                    res.add(List.of(i,j));
                 }
             }
         }
-        return result;
+        return res;
     }
-
-    private void dfs(int i, int j, int[][] heights, boolean[][] visited) {
-        if (visited[i][j]) return;
-        visited[i][j] = true;
-        for (int[] d : directions) {
-            int x = i + d[0], y = j + d[1];
-            if (x < 0 || x >= m || y < 0 || y >= n) continue;
-            if (heights[x][y] < heights[i][j]) continue;
-            dfs(x, y, heights, visited);
+    public void dfs(int row, int col, boolean[][] reachable, int[][] heights){
+        int[][] directions = new int[][]{{0, 1},{0,-1}, {1, 0}, {-1, 0}};
+        reachable[row][col]= true;
+        for(int[] dir: directions){
+            int newRow = row+dir[0];
+            int newCol = col+dir[1];
+            if(newRow<0||newRow>=heights.length||newCol<0||newCol>=heights[0].length) continue;
+            if(reachable[newRow][newCol]) continue;
+            if(heights[newRow][newCol]>=heights[row][col]){
+                dfs(newRow, newCol, reachable, heights);
+            }
+        
         }
     }
 }
