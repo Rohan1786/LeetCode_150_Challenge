eb7a6e6d80c7a6a8bf42766da82a43eb5f298f10
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-09 12:08:06 +0530

    Time: 2 ms (99.14%), Space: 45.2 MB (26.9%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,23 +1,17 @@
 class Solution {
- 
-    public void backTrack(int start, List<List<Integer>> res, List<Integer> curr, int[] nums, Set<String> set){
-          String key = curr.toString();
-
-        if (!set.contains(key)) {
-            set.add(key);
-            res.add(new ArrayList<>(curr));
+    public void backTrack(List<List<Integer>> res,   List<Integer> curr, int[] nums, int start){
+        res.add(new ArrayList<>(curr));
+        for(int i=start; i<nums.length; i++){
+            if(i>start && nums[i]==nums[i-1]) continue;
+            curr.add(nums[i]);
+            backTrack(res, curr, nums, i+1);
+            curr.remove(curr.size()-1);
         }
-      for(int i=start; i<nums.length; i++){
-        curr.add(nums[i]);
-        backTrack(i+1, res, curr, nums, set);
-        curr.remove(curr.size()-1);
-      }
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        Set<String> set = new HashSet<>();
-        backTrack(0, res, new ArrayList<>(), nums, set);
-        return res;
+         List<List<Integer>>res = new ArrayList<>();
+         Arrays.sort(nums);
+         backTrack(res, new ArrayList<>(), nums, 0);
+         return res;
     }
 }
