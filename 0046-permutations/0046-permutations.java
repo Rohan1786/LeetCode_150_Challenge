51756d143fac5c04c40c9eb52b3ccb10ff0e2784
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-09 11:18:00 +0530

    Time: 1 ms (87.06%), Space: 45.5 MB (48.55%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,22 @@
 class Solution {
-    public void backTrack(List<List<Integer>> res,
-                          List<Integer> curr,
-                          int[] nums,
-                          boolean[] used) {
-
-        if (curr.size() == nums.length) {
+    public void backTrack(List<List<Integer>> res, int[]nums, List<Integer> curr, boolean[] used){
+        if(curr.size()==nums.length){
             res.add(new ArrayList<>(curr));
             return;
         }
-
-        for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
-
-            used[i] = true;
-            curr.add(nums[i]);
-
-            backTrack(res, curr, nums, used);
-
-            curr.remove(curr.size() - 1);
-            used[i] = false;
+        for(int i=0; i<nums.length; i++){
+          if(used[i]) continue;
+          used[i]=true;
+          curr.add(nums[i]);
+          backTrack(res, nums, curr, used);
+          curr.remove(curr.size()-1);
+          used[i] = false;
         }
-    }
 
+    }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        backTrack(res, new ArrayList<>(), nums, new boolean[nums.length]);
+        backTrack(res, nums, new ArrayList<>(), new boolean[nums.length]);
         return res;
     }
 }
