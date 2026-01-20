f48bcf64aa2f5b12e655d541da837f0a16ee0d7d
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-20 16:03:03 +0530

    Time: 12 ms (59.63%), Space: 49.8 MB (30.68%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.java
+++ 0743-network-delay-time/0743-network-delay-time.java
@@ -1,37 +1,40 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-        Map<Integer, List<int[]>> graph = new HashMap<>();
-    PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-
-
-        for(int arr[]: times){
-           graph.computeIfAbsent(arr[0], x-> new ArrayList<>()).add(new int[]{arr[1], arr[2]});
+        List<List<int[]>> graph = new ArrayList<>();
+        for(int i=0; i<=n; i++){
+            graph.add(new ArrayList<>());
         }
-
-int[] distance = new int[n+1];
-Arrays.fill(distance, Integer.MAX_VALUE);
-distance[k]=0;
-pq.offer(new int[]{k, 0});
-while(!pq.isEmpty()){
-    int[] curr = pq.poll();
-    int node = curr[0];
-    int dist = curr[1];
-    if(dist!=distance[node]) continue;
-  if(graph.containsKey(node)){
-    for(int[] neighbour: graph.get(node)){
-        int nextNode = neighbour[0];
-        int nextDist = neighbour[1];
-        int newDist = nextDist+dist;
-        if(newDist< distance[nextNode]){
-            distance[nextNode]=newDist;
-            pq.offer(new int[]{nextNode, newDist});
+        for(int[] edge : times){
+            int u = edge[0];
+            int v = edge[1];
+            int w = edge[2];
+            graph.get(u).add(new int[]{v, w});
         }
-
-    }
-  }
-}
-
-int max = Arrays.stream(distance).skip(1).max().getAsInt();
-return max==Integer.MAX_VALUE?-1:max;
+        int[] dist =new int[n+1];
+        Arrays.fill(dist,Integer.MAX_VALUE);
+        dist[k]=0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b)->a[1]-b[1]);
+        pq.offer(new int[]{k, 0});
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int node = curr[0];
+            int d = curr[1];
+            if(d>dist[node]){
+                continue;
+            }
+        
+                for(int[]neigh: graph.get(node)){
+                    int currNode = neigh[0];
+                    int weight = neigh[1];
+                    if(d+weight<dist[currNode]){
+                        dist[currNode]=d+weight;
+                         pq.offer(new int[]{currNode, dist[currNode]});
+                    }
+                   
+                }
+            
+        }
+        int max =Arrays.stream(dist).skip(1).max().getAsInt();
+        return max==Integer.MAX_VALUE?-1:max;
     }
 }
