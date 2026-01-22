67fb3b3907ddd56aca2276729a0f8e07287d7046
Rohan2786 <123477897+Rohan1786@users.noreply.github.com>
2026-01-22 07:44:39 +0530

    Time: 26 ms (33.35%), Space: 50.1 MB (5.07%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,37 +1,17 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-    //     int cnt=0;
-    //    for(int i=0; i<nums.length; i++){
-    //     int value = nums[i];
-    //     if(value==k){
-    //         cnt++;
-    //     }
-    //     for(int j=i+1; j<nums.length; j++){ 
-    //          value+=nums[j];
-    //         if(k==value){
-    //             cnt++;
-    //         }
-           
-    //     }
-    //    }
-    //     return cnt;
-
-
-
-    //NOw as par the hashMap we can able to find out the subarray sub easily
-    Map<Integer, Integer> map = new HashMap<>();
-   int sum =0;
-   int count=0;
-   map.put(0, 1);
-   for(int num: nums){
+  int n =nums.length;
+  Map<Integer, Integer> map = new LinkedHashMap<>();
+  int sum =0;
+  int count =0;
+  map.put(0,1);
+  for(int num: nums){
     sum+=num;
     if(map.containsKey(sum-k)){
         count+=map.get(sum-k);
     }
-   
-        map.put(sum, map.getOrDefault(sum, 0)+1);
-     
-   }
-   return count;
+    map.put(sum, map.getOrDefault(sum, 0)+1);
+  }
+  return count;
     }
 }
